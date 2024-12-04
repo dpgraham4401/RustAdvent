b14a224d8beb0e868b77aa7c7b37a1807391c7c7
David Paul Graham <dpgraham4401@gmail.com>
2024-12-03 21:28:20 -0500

    day 3 casting values

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+mod nice;
+
 pub fn main() {
     let gift_message = String::from("Merry Christmas! Enjoy your gift!");
     attach_message_to_present(&gift_message);
 
     println!("{}", gift_message);
+
+    let good_deeds = 10;
+    let bad_deeds = 2;
+    let is_nice = nice::is_nice(good_deeds, bad_deeds);
+    println!("Is the child nice? {}", is_nice);
 }
 
 pub fn attach_message_to_present(message: &String) {
