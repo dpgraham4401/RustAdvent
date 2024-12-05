71111c57389dcd589a904fcba6d0186c6145d11a
David Graham <dpgraham4401@gmail.com>
2024-12-04 20:01:44 -0500

    day 4 start

--- src/nice.rs
+++ src/nice.rs
@@ -1,17 +1,34 @@
-// We need to find the nice and naughty kids for santa
-
-// Each good deed is worth 1 point and each bad deed is worth 2 points
 pub const GOOD_WEIGHT: f32 = 1.0;
 pub const BAD_WEIGHT: f32 = 2.0;
 
-pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
-    // Calculate the ratio of good deeds to total deeds
-    // Any ratio greater than or equal to 0.75 is considered nice
-    // e.g. 10 good deeds and 2 bad deeds =
-    // (10 * 1) / ((10 * 1) + (2 * 2)) = 10 / 14 = 0.714... (not nice)
-    // If both good and bad deeds are 0, the child is naughty
+#[derive(Debug, PartialEq)] // needed for tests
+pub enum Niceness {
+    // Create the enum variants `Nice` and `Naughty`
+    // Variant `Nice` is a tuple struct that holds the number of good deeds
+}
 
-    let total = (good_deeds as f32 * GOOD_WEIGHT)
-        / ((good_deeds as f32 * GOOD_WEIGHT) + (bad_deeds as f32 * BAD_WEIGHT));
-    total >= 0.75
+pub struct Kid {
+    // Add a field `name` of type `String`
+    // and `niceness` field of type `Niceness`
+    // Make all fields public
+}
+
+// Move yesterday's function to an associated function in the struct
+pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
+    if good_deeds == 0 && bad_deeds == 0 {
+        return false;
+    }
+
+    let good_deeds = good_deeds as f32 * GOOD_WEIGHT;
+    let bad_deeds = bad_deeds as f32 * BAD_WEIGHT;
+
+    let ratio = good_deeds / (good_deeds + bad_deeds);
+
+    ratio >= 0.75
+}
+
+impl Kid {
+    pub fn new(name: String, good_deeds: u32, bad_deeds: u32) -> Kid {
+        // Return a Kid instance
+    }
 }
